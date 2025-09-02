e51e545644c8ffa595c00817de95da094f511622
petrasvestartas <petrasvestartas@gmail.com>
2025-09-02 08:19:37 +0200

    CLEANUP

--- session_cpp/src/core.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "globals.hpp"
-#include "line.hpp"  // includes globals, point, vector
-#include "logger.hpp"
-#include "matrix.hpp"  // includes n x n matrix, transformation matrices are stored in xform static methods
-#include "plane.hpp"   // includes globals, point, vector
-#include "point.hpp"   // includes globals
-#include "vector.hpp"  // includes globals
--- session_cpp/src/globals.cpp
+++
@@ -1,8 +0,0 @@
-#include "globals.hpp"
-
-namespace geo {
-
-double GLOBALS::SCALE = 1e6;
-double GLOBALS::ANGLE = 0.11;
-
-}  // namespace geo
